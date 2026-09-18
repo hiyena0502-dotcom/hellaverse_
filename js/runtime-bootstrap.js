f253261c96074fc6f7e2efd935baf98e21e07a4e
hiyena0502-dotcom <hiyena0502@gmail.com>
2026-09-19 04:01:07 +0900

    Load 50-scene Hazbin fandom dialogue expansion

--- js/runtime-bootstrap.js
+++ js/runtime-bootstrap.js
@@ -97,6 +97,7 @@ await loadClassic('js/room-exit-transition.js?v=4');
 await loadStableCore();
 await loadClassic('js/event-progression-runtime-v1.js?v=2');
 await loadClassic('js/character-ambient-100-runtime-v1.js?v=7');
+await loadClassic('js/hazbin-fanon-dialogue-50-v1.js?v=1');
 
 await loadClassic('js/dialogue-token-renderer.js?v=7');
 await loadClassic('js/dialogue-single-beat-runtime-v23.js?v=2');
